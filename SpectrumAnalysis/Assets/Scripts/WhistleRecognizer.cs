c308cee3a5f5867e559b889e1d6a3090a83ebf5b
Gonzalo <gonalba@ucm.es>
2021-07-21 14:12:57 +0200

    [SAP] Detección de silbido implementada (especificando frecuencia y duración)

--- SpectrumAnalysis/Assets/Scripts/WhistleRecognizer.cs
+++ SpectrumAnalysis/Assets/Scripts/WhistleRecognizer.cs
@@ -17,43 +17,44 @@ namespace PatternRecognizer
         private int windowSizeSmall = 20;
 
         // cuenta de las veces que se detecta una frecuencia y un silencio respectivamente
-        private uint countFrecuencyDetected = 0;
+        private uint countFrequencyDetected = 0;
         private uint countSilenceDetected = 0;
+        // cuenta las veces que la frecuencia no es la buscada (se usa porque al silbar no siempre aguantamos la misma frecuencia)
+        private uint countWrogFrequencyDetected = 0;
+        private uint maxWrongRangeFrequency = 10;
 
         // rango de error al comprobar la duracion y la frecuencia del silbido 
         private float offsetDuration = 0.1f;
-        private float offsetFrequency;
+        private float offsetFrequency = 0.01f;
+        // minima intensidad que tiene que detectar para que contabilice como que ha habido un silbido
+        private int minIntensityDetection = 15;
+
+        // maxima y minima intensidad de toda la muestra. Valores acordes con los de un silbido
+        private int maxAllIntensity = 80;
+        private int minAllIntensity = 20;
         #endregion
 
         #region PUBLIC_ATTRIBUTES
-        // minima intensidad que tiene que detectar para que contabilice como que ha habido un silbido
-        public int minIntensityDetection = 0;
-
-        // maxima y minima intensidad de toda la muestra. Valores acordes con los de un silbido
-        public int maxAllIntensity = 0;
-        public int minAllIntensity = 0;
-
         // Frecuencia que tiene que ser identificada y su duracion
         public uint frequency;
         public uint frequencyDuration;
         #endregion
 
-
+        // debug
         public int debugFrequency;
+        // debug
 
         /*
          * Primera criba en la intensidad. Establecemos un valor maximo y minimo (en ambos tamaños de ventana)
          * 
-         * Identificacion de la frecuencia. El resultado final es que se "grabara" el silbido y hay que identificar 
-         * y guardar el rango de frecuencias donde se encuentra. El método Recognize(...) lo que hace es identificar si esta dentro de ese rango.
-         * Tambien guardar el tiempo que dura la frecuencia (tambien en forma de rango para tener menor tasa de fallo)
+         * Identificacion de la frecuencia. El resultado final es que se "grabara" el silbido (otra clase por hacer) y hay que identificar 
+         * y guardar la frecuencia. El método Recognize(...) lo que hace es identificar si se silba en la frecuencia grabada.
+         * Tambien guardar el tiempo que dura la frecuencia (tanto la frecuencia como la duracion tienen un margen de error)
          * 
-         * Tambien identificar ascendentes y descendentes. De esta forma, cuando se cambie de frecuencia, se guarda esa frecuencia 
-         * y el tiempo que ha durado y se pone a contar el tiempo de la nueva frecuancia.
          * **/
         private void Start()
         {
-            offsetFrequency = 0.01f * SoundEventManager.Instance.GetResolution();
+            offsetFrequency *= SoundEventManager.Instance.GetResolution();
         }
 
         public override bool Recognize(float[] array)
@@ -61,38 +62,56 @@ namespace PatternRecognizer
             bool isClick = false;
 
             // ventana deslizante con tamaño de ventana grande
-            WindowUnit maxBig = SlidingWindow(array, windowSizeBig);
+            WindowUnit maxBigSize = SlidingWindow(array, windowSizeBig);
             WindowUnit allFrequencies = SlidingWindow(array, array.Length - 1);
 
-            //Debug.Log((int)max.intensity);
-            //Debug.Log((int)maxBig.intensity + " - " + (int)allFrequencies.intensity);
-            // Si la intensidad supera un limite asumimos que estamos oyendo un chasquido
-            if (maxBig.intensity > minIntensityDetection && allFrequencies.intensity > minAllIntensity && allFrequencies.intensity < maxAllIntensity)
+            // Si la intensidad esta entre cierto rango, entonces estamos oyendo un posible silbido
+            if (maxBigSize.intensity > minIntensityDetection && allFrequencies.intensity > minAllIntensity && allFrequencies.intensity < maxAllIntensity)
             {
-                WindowUnit maxSmall = SlidingWindow(array, windowSizeSmall);
+                // pasamos una ventana mas pequeña para identificar la frecuencia exacta
+                WindowUnit maxSmallSize = SlidingWindow(array, windowSizeSmall);
 
-                // Comprobamos que fecuencia del input esta dentro del rango de la fecuencia buscada. Si no, reiniciamos el contador
-                if (maxSmall.frequency < (frequency + offsetFrequency) && maxSmall.frequency > (frequency - offsetFrequency))
+                //debug
+                debugFrequency = maxSmallSize.frequency;
+                //debug
+
+                // Comprobamos que fecuencia del input esta dentro del rango de la fecuencia buscada
+                if ((maxSmallSize.frequency < (frequency + offsetFrequency) && maxSmallSize.frequency > (frequency - offsetFrequency)))
                 {
-                    countFrecuencyDetected++;
-                    debugFrequency = maxSmall.frequency;
+                    countFrequencyDetected++;
+                    countWrogFrequencyDetected = 0;
+                    countSilenceDetected = 0;
                 }
+                // no detecta la frecuencia pero hay cierto margen de error al detectar la frecuencia porque el silbido tiene fluctuaciones
+                else if (countWrogFrequencyDetected < maxWrongRangeFrequency)
+                {
+                    countWrogFrequencyDetected++;
+                    countFrequencyDetected++;
+                    countSilenceDetected = 0;
+                }
+                // si supera el margen de error entonces asumimos que no está silbando en la frecuencia adecuada
                 else
-                    countFrecuencyDetected = 0;
+                {
+                    countFrequencyDetected = 0;
+                    countWrogFrequencyDetected = 0;
+                    countSilenceDetected++;
+                }
 
-                countSilenceDetected = 0;
             }
-            else if (maxBig.intensity < minIntensityDetection && allFrequencies.intensity < minAllIntensity)
+            // si la intensidad es demasiado baja reiniciamos contadores y contabilizamos como silencio
+            else if (maxBigSize.intensity < minIntensityDetection && allFrequencies.intensity < minAllIntensity)
             {
                 countSilenceDetected++;
-                countFrecuencyDetected = 0;
+                countFrequencyDetected = 0;
+                countWrogFrequencyDetected = 0;
             }
 
             // Comprobamos que la duracion de la frecuencia esta dentro del rango establecido
-            if(countFrecuencyDetected < (frequencyDuration + (frequencyDuration*offsetDuration)) && 
-               countFrecuencyDetected > (frequencyDuration - (frequencyDuration * offsetDuration)))
+            if (countFrequencyDetected < (frequencyDuration + (frequencyDuration * offsetDuration)) &&
+               countFrequencyDetected > (frequencyDuration - (frequencyDuration * offsetDuration)))
             {
                 isClick = true;
+                countFrequencyDetected = 0;
             }
 
 
